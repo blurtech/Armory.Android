4e59906ba23df4edec7c90c2c02f9d46094e11e9
srggrch <srg.grch@gmail.com>
2021-03-16 18:01:44 +0700

    TrueTIme

--- app/src/main/java/tech/blur/armory/ArmoryApp.kt
+++ app/src/main/java/tech/blur/armory/ArmoryApp.kt
@@ -1,6 +1,7 @@
 package tech.blur.armory
 
 import android.app.Application
+import io.tempo.Tempo
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 import tech.blur.armory.di.*
@@ -8,6 +9,8 @@ import tech.blur.armory.di.*
 class ArmoryApp: Application() {
     override fun onCreate() {
         super.onCreate()
+
+        Tempo.initialize(this)
 
         startKoin {
             androidContext(applicationContext)
---
+++ app/src/main/java/tech/blur/armory/common/TrueTime.kt
@@ -0,0 +1,20 @@
+package tech.blur.armory.common
+
+import com.soywiz.klock.DateTime
+import com.soywiz.klock.DateTimeTz
+import com.soywiz.klock.TimezoneOffset
+import io.tempo.Tempo
+
+class TrueTime {
+     companion object {
+         fun unixTime() = Tempo.nowOrNull() ?: DateTime.nowUnixLong()
+
+         fun nowLocal(timezoneOffset: Double): DateTimeTz {
+             return DateTime(unixTime()).toOffset(TimezoneOffset(timezoneOffset))
+         }
+
+         fun nowUtc(): DateTime {
+             return DateTime(unixTime())
+         }
+    }
+}
